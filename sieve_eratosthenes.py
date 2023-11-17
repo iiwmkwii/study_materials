e272973bb538872b3ae69b5b45ff946f68f083db
WMKW <nikolmerkulov@gmail.com>
2023-11-16 21:58:39 -0800

    sieve completed

--- sieve_eratosthenes.py
+++ sieve_eratosthenes.py
@@ -1,3 +1,17 @@
-print('Hello world')
+def sieve_eratosthenes():
+    """ Сито Эратосфена,
+    функция получает на вход натрульное число N
+    выводит все числа от 0 до N с указанием простое
+    или составное"""
+    n = int(input())
+    a = [True] * n
+    a[0] = a[1] = False
+    for k in range(2, n):
+        if a[k]:
+            for m in range(2 * k, n, k):
+                a[m] = False
+    for k in range(n):
+        print(k, '-', 'простое' if a[k] else 'составное')
 
-print('6')
+
+sieve_eratosthenes()
